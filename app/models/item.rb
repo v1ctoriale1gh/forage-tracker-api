2217b6daad6eb30d3e306983053fdfa46962a0f5
github username <victorialeigh.tech@gmail.com>
2020-11-22 18:48:08 -0500

    Item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,7 @@
 class Item < ApplicationRecord
+    has_many :harvests
+    has_many :expeditions, through: :harvests
+    
+    validates :name, :description, presence: true
+    validates :name, uniqueness: true
 end
