89e9a618237a091615f97fbd57adee5cbcb77021
github username <victorialeigh.tech@gmail.com>
2020-11-22 18:47:37 -0500

    harvest model

--- app/models/harvest.rb
+++ app/models/harvest.rb
@@ -1,2 +1,7 @@
 class Harvest < ApplicationRecord
+    belongs_to :expedition
+    belongs_to :item
+    accepts_nested_attributes_for :item, reject_if: proc { |attributes| attributes['name'].blank? }
+    
+    validates :amount, presence: true
 end
