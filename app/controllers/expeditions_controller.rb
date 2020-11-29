60e47f9eb5197d91c3ed84e30810c43a3cb79fa9
github username <victorialeigh.tech@gmail.com>
2020-11-29 17:25:21 -0500

    redoing controllers

--- app/controllers/expeditions_controller.rb
+++ app/controllers/expeditions_controller.rb
@@ -1,11 +1,4 @@
 class ExpeditionsController < ApplicationController
-    before_action :require_login
-    before_action :set_expedition, only: [:show, :edit, :update, :destroy]
-    before_action :set_user
 
-    def show
-        allowed_user(@user, :show)
-        #byebug
-    end
 
 end
