50a6e941848896cc2b35a2bb8e954a2b4487bec6
github username <victorialeigh.tech@gmail.com>
2020-11-29 16:06:42 -0500

    working on application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,29 @@
 class ApplicationController < ActionController::API
+    helper_method :logged_in?, :require_login, :current_user, :allowed_user
+    
+    def current_user
+      #byebug
+      current_user||= User.find_by(:id => session[:user_id]) #if !!session[:user_id] 
+    end
+
+      def logged_in?
+        #byebug
+        !!session[:user_id]
+      end
+
+      def require_login
+        unless logged_in?
+            #byebug
+            render json: { error: 'Please log in' }
+        end
+        end
+
+    def allowed_user(params_user, rendee)
+     if current_user == params_user
+      #byebug
+        render json: rendee 
+     else 
+        render json: { error: '' }
+      end
+    end
 end
--- app/controllers/expeditions_controller.rb
+++ app/controllers/expeditions_controller.rb
@@ -1,2 +1,3 @@
 class ExpeditionsController < ApplicationController
+    
 end
